3d8acd5439f40a138f6e7a997ffd51252dbcdb83
Mohammad Sagor Ali <sagorbbr1@gmail.com>
2025-05-13 22:16:45 +0600

    connect with db

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
+
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+mongoose.connect(process.env.MONGO_URI);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Meal App API running");
 });
 
 app.listen(port, () => {
