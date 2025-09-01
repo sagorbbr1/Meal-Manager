cf38168dea46f01cfa49ff7a64757bb3ce84be6b
Mohammad Sagor Ali <sagorbbr1@gmail.com>
2025-09-01 16:52:09 +0600

    update db

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
+    await mongoose.connect(process.env.MONGO_URI); // no extra options needed
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.error("❌ MongoDB connection error:", err.message);
